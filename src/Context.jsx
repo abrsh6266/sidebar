d146e7fdd365d4e3371675c4f7fe84cea07fd9db
Abrham <abrsh6266@gmail.com>
2024-02-20 01:59:37 -0900

    wrapping the application with context Api

---
+++ src/Context.jsx
@@ -0,0 +1,38 @@
+import { useState, createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+        isModalOpen,
+        isSidebarOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  useContext(AppContext);
+};
